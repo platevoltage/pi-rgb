120a81736b3fe0df14534a4210de955b33d47ca1
antieatingactivist <antieatingactivist@gmail.com>
2023-04-14 01:14:25 -0700

    initial commit

---
+++ led.py
@@ -0,0 +1,25 @@
+import board
+import neopixel
+import time, psutil
+
+pixels = neopixel.NeoPixel(board.D18, 1)
+pixels.fill((0, 0, 0))
+oldLoad = 0;
+newLoad = 0;
+
+while True:
+
+    newLoad = round(psutil.cpu_percent())
+
+    time.sleep(.5)
+
+    while oldLoad != newLoad:
+        if oldLoad < newLoad:
+            oldLoad += 1
+        else:
+            oldLoad -= 1
+        pixels.fill((oldLoad*2.55, oldLoad/2, 255 - oldLoad*2.55))
+        # pixels.fill((0, 0, 0))
+        print(oldLoad, newLoad)
+        time.sleep(.02)
+        
